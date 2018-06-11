b898d0a1dd34658a719a3c0c4551bfc3bc825f84
abiscayart <agustinabiscayart@gmail.com>
2018-06-11 10:08:06 -0300

    test pivotal config-server

---
+++ src/main/java/com/example/testgreetingconfig/TestGreetingConfigApplication.java
@@ -0,0 +1,12 @@
+package com.example.testgreetingconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGreetingConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGreetingConfigApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/testgreetingconfig/quote/Quote.java
@@ -0,0 +1,14 @@
+package com.example.testgreetingconfig.quote;
+
+public class Quote {
+    private String text;
+    private String author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
